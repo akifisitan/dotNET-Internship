5185d392e76cfe31bf884ba77c1fd366bfe0d58a
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-07-26 17:55:54 +0300

    v2

---
+++ SummerSchool/Infrastructure/SummerSchool.DataAccess.MsSql/DbContext/SummerSchoolDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ef = Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SummerSchool.Entity.Entity;
+
+namespace SummerSchool.DataAccess.MsSql.DbContext
+{
+    public class SummerSchoolDbContext : ef.DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public SummerSchoolDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ SummerSchool/Infrastructure/SummerSchool.DataAccess.MsSql/Migrations/20230726110517_userRelationAdded.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SummerSchool.DataAccess.MsSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class userRelationAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Books");
+        }
+    }
+}
